2f062e2f537625480160d522ba579a0eb18aea44
Will Simpson <wsimpson@etsy.com>
2022-12-05 23:24:30 -0700

    chore: add day 5 part 1

--- 1.2/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/simpsonw/aoc-2022
-
-go 1.19
---
+++ 5.1/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"github.com/simpsonw/aoc-2022/utils"
+	"regexp"
+	"strings"
+)
+
+var columns [][]string
+
+func main() {
+	lines := utils.GetLines()
+	var startedMoveInstructions = false
+	for _, l := range lines {
+		if l == "" {
+			continue
+		}
+		if !startedMoveInstructions && isNumberLine(l) {
+			startedMoveInstructions = true
+			continue
+		}
+		if startedMoveInstructions {
+			moveCrates(l)
+		} else {
+			scanCrates(l)
+		}
+	}
+
+	fmt.Println("Crates on the tops of stacks:")
+
+	for _, v := range columns {
+		fmt.Printf("%s,", v[len(v)-1])
+	}
+
+	fmt.Println("")
+}
+
+func printColumns() {
+	for k, v := range columns {
+		fmt.Printf("Column %d:", k+1)
+		for _, v1 := range v {
+			fmt.Printf(" %s", v1)
+		}
+		fmt.Println("")
+	}
+}
+
+func moveCrates(l string) {
+	var qty, to, from int
+	_, err := fmt.Sscanf(l, "move %d from %d to %d", &qty, &from, &to)
+	if err != nil {
+		panic(err)
+	}
+	from--
+	to--
+	var fromIndex = len(columns[from]) - qty
+	for i := len(columns[from]) - 1; i >= fromIndex; i-- {
+		columns[to] = append(columns[to], columns[from][i])
+	}
+	columns[from] = columns[from][:fromIndex]
+}
+
+func isNumberLine(l string) bool {
+	var r = regexp.MustCompile(`(\s[0-9]\s)(\s)?`)
+	var matches = r.FindAllString(l, -1)
+	for k, v := range matches {
+		matches[k] = strings.TrimSpace(v)
+	}
+	return len(matches) > 0
+}
+
+func scanCrates(l string) {
+	var r = regexp.MustCompile(`([^0-9]{3})(\s)?`)
+	var matches = r.FindAllString(l, -1)
+	if len(columns) == 0 {
+		columns = make([][]string, len(matches))
+	}
+	for k, v := range matches {
+		var crate = strings.TrimSpace(v)
+		if crate != "" {
+			columns[k] = append([]string{crate}, columns[k]...)
+		}
+	}
+}
